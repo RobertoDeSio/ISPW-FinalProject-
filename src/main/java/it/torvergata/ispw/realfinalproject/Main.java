5e45dccb40a25c3e0903fcd08c265a1afe2ffe45
Roberto <2003.rds@gmail.com>
2025-12-29 16:52:03 +0100

    test

---
+++ src/main/java/it/torvergata/ispw/realfinalproject/Main.java
@@ -0,0 +1,17 @@
+package it.torvergata.ispw.realfinalproject;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module it.torvergata.ispw.realfinalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.torvergata.ispw.realfinalproject to javafx.fxml;
+    exports it.torvergata.ispw.realfinalproject;
+}
